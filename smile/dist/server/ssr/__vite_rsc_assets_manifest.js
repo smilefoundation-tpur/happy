f901a5af6aaac5e48e1254fb9a615cad46027937
smilefoundation-tpur <indra.learn59@gmail.com>
2026-07-03 13:22:48 -0400

    Add files via upload
    Initial Webpage.

---
+++ smile/dist/server/ssr/__vite_rsc_assets_manifest.js
@@ -0,0 +1,38 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-CUCXCMH_.js\")",
+  "clientReferenceDeps": {
+    "e565d5eea707": {
+      "js": [
+        "/assets/index-CUCXCMH_.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "01975f39a216": {
+      "js": [
+        "/assets/layout-segment-context-3aCkhsGn.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-CUCXCMH_.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "c40599a955d2": {
+      "js": [
+        "/assets/index-CUCXCMH_.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-Ciluro_y.css"
+      ]
+    }
+  }
+}
